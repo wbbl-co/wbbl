c7563c7ba6f1cafa97e2b59c85cc558cea83a7c6
Natalie Cuthbert <natalie@cuthbert.co.za>
2024-03-08 16:36:39 +0200

    Added frontend entry points

---
+++ web/App.tsx
@@ -0,0 +1,52 @@
+import {
+  ReactFlow,
+  Controls,
+  Background,
+  MiniMap,
+  useNodesState,
+  useEdgesState,
+} from "@xyflow/react";
+import React from "react";
+import "@xyflow/react/dist/style.css";
+
+const initNodes = [
+  {
+    id: "a",
+    data: { label: "Node A" },
+    position: { x: 250, y: 0 },
+  },
+  {
+    id: "b",
+    data: { label: "Node B" },
+    position: { x: 100, y: 100 },
+  },
+];
+
+const initEdges = [
+  {
+    id: "a-b",
+    source: "a",
+    target: "b",
+  },
+];
+
+function App() {
+  const [nodes, , onNodesChange] = useNodesState(initNodes);
+  const [edges, , onEdgesChange] = useEdgesState(initEdges);
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      onNodesChange={onNodesChange}
+      edges={edges}
+      onEdgesChange={onEdgesChange}
+      fitView
+    >
+      <Background />
+      <Controls />
+      <MiniMap />
+    </ReactFlow>
+  );
+}
+
+export default App;

a2c8a7beaeef6669cccd5b65429975715717d267
Natalie Cuthbert <natalie@cuthbert.co.za>
2024-03-30 09:20:20 +0200

    Fixed loading screen and fixed visual bug with port available glow in light mode

--- web/components/LoadingScreen.tsx
+++ web/components/LoadingScreen.tsx
@@ -1,7 +1,9 @@
+import { Heading } from "@radix-ui/themes";
+
 export default function LoadingScreen() {
   return (
-    <div style={{ display: "flex", height: '100%', width: "100%", alignItems: "center", justifyItems: "center", background: "black", fontSize: "2rem", color: "white", fontStyle: "italic" }}>
-      <h1>Loading...</h1>
+    <div style={{ display: "flex", height: "100vh", width: "100vw", alignItems: "center", justifyItems: "center", background: "var(--background)", fontSize: "2rem", color: "white", fontStyle: "italic" }}>
+      <Heading size={"8"} style={{ width: '100%', fontFamily: 'var(--brand-font-family)', fontWeight: 'normal' }} color="lime" align={'center'} as="h1">Loading...</Heading>
     </div>
   );
 }

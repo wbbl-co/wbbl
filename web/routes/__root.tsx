e87677b295af0c32256056db583d04091dd839cb
Natalie Cuthbert <natalie@cuthbert.co.za>
2024-05-16 16:20:23 +0200

    Removed router devtools

--- web/routes/__root.tsx
+++ web/routes/__root.tsx
@@ -1,20 +1,9 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
     component: () => (
         <>
-            <div className="p-2 flex gap-2">
-                <Link to="/app" className="[&.active]:font-bold">
-                    Home
-                </Link>{' '}
-                <Link to="/app/about" className="[&.active]:font-bold">
-                    About
-                </Link>
-            </div>
-            <hr />
             <Outlet />
-            <TanStackRouterDevtools />
         </>
     ),
 })

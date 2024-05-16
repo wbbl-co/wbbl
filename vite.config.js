59c4fd4491b83748ee8c372d38cef052f13eca8a
Natalie Cuthbert <natalie@cuthbert.co.za>
2024-05-16 15:56:27 +0200

    Added tan-stack react router to project

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait()],
+  plugins: [
+    TanStackRouterVite({
+      routesDirectory: './web/routes',
+      generatedRouteTree: './web/routeTree.gen.ts'
+    }),
+    wasm(),
+    topLevelAwait()
+  ],
   base: "/app",
   worker: {
     // Not needed with vite-plugin-top-level-await >= 1.3.0
